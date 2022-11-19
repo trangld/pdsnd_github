02447abf6f7f601c1323c50c46b02bb063cd76eb
kelly <kelly@fsoft.com.vn>
2022-11-19 16:15:39 +0700

    refactor code

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,6 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
 MONTH_DATA = ['january', 'february', 'march', 'april', 'may', 'june', 'all']
 DAY_DATA = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'all']
 
